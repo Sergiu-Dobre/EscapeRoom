c90c7fb38c1e30e94ec9f9950fdaf558a785b376
Karina-Blur <k.blurtsyan@st.hanze.nl>
2023-05-13 13:36:54 +0200

    text

--- Assets/Scripts/pipescript.cs
+++ Assets/Scripts/pipescript.cs
@@ -22,8 +22,8 @@ public class pipescript : MonoBehaviour
     private void Start()
     {
         PossibleRots = correctRotation.Length;
-        int rand = Random.Range(0, rotations.Length);
-        transform.eulerAngles = new Vector3(0, -90, rotations[rand]);
+        //int rand = Random.Range(0, rotations.Length);
+        //transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
 
         if(PossibleRots > 1)
         {
